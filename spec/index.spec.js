e66fae8870981890657db346238727d267737799
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2018-02-07 00:45:08 +0900

    chore: initialize project for rxjs

---
+++ spec/index.spec.js
@@ -0,0 +1,7 @@
+"use strict";
+describe('sandbox test', function () {
+    it('for console', function () {
+        console.log('test');
+    });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguc3BlYy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluZGV4LnNwZWMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLFFBQVEsQ0FBQyxjQUFjLEVBQUU7SUFDdkIsRUFBRSxDQUFDLGFBQWEsRUFBRTtRQUNoQixPQUFPLENBQUMsR0FBRyxDQUFDLE1BQU0sQ0FBQyxDQUFDO0lBQ3RCLENBQUMsQ0FBQyxDQUFDO0FBQ0wsQ0FBQyxDQUFDLENBQUMifQ==
---
+++ spec/index.spec.ts
@@ -0,0 +1,5 @@
+describe('sandbox test', () => {
+  it('for console', () => {
+    console.log('test');
+  });
+});
