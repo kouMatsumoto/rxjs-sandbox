f88152a07df608622b000cefed133e9780736e60
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2018-02-07 01:12:37 +0900

    test: add case when ordinary subscribing

--- spec/index.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('sandbox test', function () {
-    it('for console', function () {
-        console.log('test');
-    });
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguc3BlYy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluZGV4LnNwZWMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLFFBQVEsQ0FBQyxjQUFjLEVBQUU7SUFDdkIsRUFBRSxDQUFDLGFBQWEsRUFBRTtRQUNoQixPQUFPLENBQUMsR0FBRyxDQUFDLE1BQU0sQ0FBQyxDQUFDO0lBQ3RCLENBQUMsQ0FBQyxDQUFDO0FBQ0wsQ0FBQyxDQUFDLENBQUMifQ==
--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,5 +1,25 @@
+import { Subject } from 'rxjs/Subject';
+
 describe('sandbox test', () => {
-  it('for console', () => {
-    console.log('test');
+  let subject: Subject<null>;
+
+  beforeEach(() => {
+    subject = new Subject();
+  });
+
+  afterEach(() => {
+    if (subject && !subject.closed) {
+      subject.complete();
+    }
+  });
+
+  it('should count observers on ordinary subscribe handling', () => {
+    expect(subject.observers.length).toBe(0);
+
+    const subscription = subject.subscribe();
+    expect(subject.observers.length).toBe(1);
+
+    subscription.unsubscribe();
+    expect(subject.observers.length).toBe(0);
   });
 });
